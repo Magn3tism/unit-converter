bab5209d85e42e26fe4a26d687e6c52e432dabd6
magn3tism <chapagainsakshyam.com>
2023-04-09 07:34:48 +0545

    Add input field and button

--- converter.py
+++ converter.py
@@ -8,9 +8,17 @@ window.geometry("300x150")
 # window.attributes('-type', 'dialog')
 
 # Title
-title_label = ttk.Label(master = window, text = "Miles to Kilometers", font="Calibri 24")
+title_label = ttk.Label(master = window, text = "Miles to Kilometers", font="Calibri 24 bold")
 title_label.pack()
 
+# Input
+input_frame = ttk.Frame(master = window)
+entry = ttk.Entry(master = input_frame)
+button = ttk.Button(master = input_frame, text = "Convert")
+
+entry.pack()
+button.pack()
+input_frame.pack()
 
 # Run
 window.mainloop()
