ccf9e74726f7a271c639577a1ff6c878c99b66f2
magn3tism <chapagainsakshyam.com>
2023-04-09 07:39:09 +0545

    Add output label

--- converter.py
+++ converter.py
@@ -9,16 +9,20 @@ window.geometry("300x150")
 
 # Title
 title_label = ttk.Label(master = window, text = "Miles to Kilometers", font="Calibri 24 bold")
-title_label.pack()
+title_label.pack(pady  = 10)
 
 # Input
 input_frame = ttk.Frame(master = window)
 entry = ttk.Entry(master = input_frame)
 button = ttk.Button(master = input_frame, text = "Convert")
 
-entry.pack()
-button.pack()
+entry.pack(side = "left", padx = 10)
+button.pack(side = "left")
 input_frame.pack()
+
+# Output
+output_label = ttk.Label(master = window, text = "Output", font = "Calibri 24")
+output_label.pack(pady = 10)
 
 # Run
 window.mainloop()
